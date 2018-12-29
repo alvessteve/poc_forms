2a3c870601ad7a553c86069cc0c7af875eb4cdbd
Steve <salves@xebia.com>
2018-12-29 15:25:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# forms_poc
+research on some forms
+
+Here the watsi form: https://watsi.org/universal-fund/join?utf8=%E2%9C%93&donation_amount=30
---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById('tooltipAc').addEventListener('click', function() {
+    document.getElementById('dropdownMoney').className += " open";
+})
